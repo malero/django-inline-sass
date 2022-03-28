29de723ed19b39994ded36f42e590458e74cf64e
Matt <contact@maleero.com>
2022-03-28 08:10:17 -0500

    Enabled caching

--- django_inline_sass/templatetags/inline_sass.py
+++ django_inline_sass/templatetags/inline_sass.py
@@ -1,6 +1,9 @@
+import hashlib
+
 from django import template
 from django.conf import settings
 from django.template.utils import get_app_template_dirs
+from django.core.cache import cache
 
 register = template.Library()
 
@@ -18,8 +21,19 @@ class SASSNode(template.Node):
 
     def render(self, context):
         import sass as libsass
-        return libsass.compile(
-            string=self.nodelist.render(context),
-            output_style=getattr(settings, 'DEBUG', False) and 'compressed' or 'nested',
-            include_paths=[str(path) for path in get_app_template_dirs('sass')]
-        )
+        cache_enabled = getattr(settings, 'DJANGO_INLINE_SASS_CACHE', False)
+        sass_string = self.nodelist.render(context)
+        cache_key = 'django_inline_sass_' + hashlib.md5(sass_string.encode('utf-8')).hexdigest()
+        dir = getattr(settings, 'DJANGO_INLINE_SASS_DIR', 'sass')
+        css = cache_enabled and cache.get(cache_key) or None
+
+        if not css:
+            css = libsass.compile(
+                string=sass_string,
+                output_style=getattr(settings, 'DEBUG', False) and 'compressed' or 'nested',
+                include_paths=[str(path) for path in get_app_template_dirs(dir)]
+            )
+            if cache_enabled:
+                cache.set(cache_key, css, getattr(settings, 'DJANGO_INLINE_SASS_CACHE_TIMEOUT', 60 * 60 * 24))
+
+        return css
